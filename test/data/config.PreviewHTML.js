3a07398b8601745a8c05d0a417cc7f181903501b
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2016-02-15 10:02:54 +0200

    -c CLO

---
+++ test/data/config.PreviewHTML.js
@@ -0,0 +1,6 @@
+window.MathJax = {
+    showMathMenu: false,
+    extensions: ["tex2jax.js"],
+    tex2jax: { inlineMath: [["$","$"], ["\\(","\\)"]] },
+    jax: ["input/TeX","output/PreviewHTML"]
+}
